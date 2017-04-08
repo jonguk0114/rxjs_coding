6ea842ce50bd8adb2039625ce4160b009ec19131
Jonguk Lee <peace0114@gmail.com>
2017-04-08 14:59:51 +0900

    Observable.from, arguments -> observables

--- merge_seq.js
+++ merge_seq.js
@@ -2,7 +2,7 @@ const { Observable } = require('rxjs/Rx');
 
 function mergeSeq(...observables) {
     const observableLength = observables.length;
-    return Observable.from(arguments)
+    return Observable.from(observables)
 	.mergeMap((observable,index) => Observable.zip(
 			observable.do(value => console.log(`do ${value}`)).last(), 
 			Observable.of(index), 
@@ -33,5 +33,7 @@ const req1$ = Observable.timer(2000).map(value => "req1");
 const req2$ = Observable.timer(4000).map(value => "req2");
 const req3$ = Observable.timer(2000).map(value => "req3");
 
-mergeSeq(req1$, req2$, req3$).subscribe(value => console.log(`subscribe result: ${value}`)); 
+mergeSeq(req1$, req2$, req3$).subscribe(
+	value => console.log(`subscribe result: ${value}`)
+); 
 
