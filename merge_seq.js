c9103b40956036f3f642c9093e082f323f889c79
Jonguk Lee <peace0114@gmail.com>
2017-04-08 09:55:40 +0900

    merge로 하되, 요청한 순서에 따라 다음 item을 보여준다. 요청순서가 늦으면 scan에 저장하고 있다가, 사용가능할 때 보여줌. 제약사항으로는 각 옵저버블은 take(1)로 하여 1개의 아이템만 취급한다.
    merge but emission sequence follows request sequence when response is available.
    restiction: each observable emits only one item by using take(1).

---
+++ merge_seq.js
@@ -0,0 +1,37 @@
+var Rx = require('rxjs/Rx');
+const { Observable } = Rx;
+
+function mergeSeq(...observables) {
+    const observableLength = arguments.length;
+    return Observable.from(arguments)
+	.mergeMap((observable,index) => Observable.zip(observable.do(value => console.log(`do ${value}`)).take(1), Observable.of(index), 
+			(result, index) => {return {result, index}}))
+	.scan((acc, current) => {
+	   acc.results = null;
+	   acc.accResults[current.index] = current.result;
+	   const results = [];
+	   let searchIndex;
+  	   for (searchIndex = acc.nextIndex; 
+			acc.accResults[searchIndex]; searchIndex++) {
+		results.push(acc.accResults[searchIndex]);	
+		acc.accResults[searchIndex] = null;
+ 	   }
+	   return {
+		nextIndex: searchIndex,
+		results: results,
+		accResults: acc.accResults
+	   }
+	}, {
+	   nextIndex: 0,
+	   results: null,
+	   accResults: new Array(observableLength)
+	}).concatMap((accObj) => Observable.from(accObj.results));
+}
+
+const req1$ = Observable.timer(2000).map(value => "req1");
+const req2$ = Observable.timer(4000).map(value => "req2");
+const req3$ = Observable.timer(2000).map(value => "req3");
+
+mergeSeq(req1$, req2$, req3$).subscribe(value => console.log(`subscribe result: ${value}`)); 
+
+
