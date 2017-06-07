091be36c5172bd51c42ce042be1f6809b10f03cb
Jonguk Lee <peace0114@gmail.com>
2017-06-08 00:32:26 +0900

    parallelConcatFirst parallelConcatLast, pluck and concatall, keep scan seed

--- merge_seq.js
+++ merge_seq.js
@@ -1,39 +1,37 @@
 const { Observable } = require('rxjs/Rx');
-
-function mergeSeq(...observables) {
+const mergeSeq = function(first = false, ...observables) {
     const observableLength = observables.length;
+    const selectOperatorName = first ? 'first' : 'last';
     return Observable.from(observables)
 	.mergeMap((observable,index) => Observable.zip(
-			observable.do(value => console.log(`do ${value}`)).last(), 
+		        observable.do(value => console.log(`do ${value}`))[selectOperatorName](), 
 			Observable.of(index), 
 			(result, index) => {return {result, index}}))
 	.scan((acc, current) => {
-	   acc.results = null;
-	   acc.accResults[current.index] = current.result;
+	   const accResults = acc.accResults || new Array(observableLength);
+	   accResults[current.index] = current.result;
+	   const nextIndex = acc.nextIndex;
 	   const results = [];
-	   let searchIndex;
-  	   for (searchIndex = acc.nextIndex; 
-			acc.accResults[searchIndex]; searchIndex++) {
-		results.push(acc.accResults[searchIndex]);	
-		acc.accResults[searchIndex] = null;
+	   let serachIndex;
+  	   for (searchIndex = nextIndex; accResults[searchIndex]; searchIndex++) {
+		results.push(accResults[searchIndex]);	
+		accResults[searchIndex] = null;
  	   }
 	   return {
 		nextIndex: searchIndex,
 		results: results,
-		accResults: acc.accResults
+		accResults: accResults
 	   }
-	}, {
-	   nextIndex: 0,
-	   results: null,
-	   accResults: new Array(observableLength)
-	}).concatMap((accObj) => Observable.from(accObj.results));
-}
+	}, {nextIndex: 0}).pluck('results').concatAll();
+};
+const parallelConcatFirst = (...observables) => mergeSeq(true, ...observables);
+const parallelConcatLast = (...observables) => mergeSeq(false, ...observables);
 
 const req1$ = Observable.timer(2000).map(value => "req1");
 const req2$ = Observable.timer(4000).map(value => "req2");
 const req3$ = Observable.timer(2000).map(value => "req3");
 
-mergeSeq(req1$, req2$, req3$).subscribe(
-	value => console.log(`subscribe result: ${value}`)
-); 
+mergeSeq(req1$, req2$, req3$).subscribe(value => console.log(`subscribe result: ${value}`));
+//parallelConcatLast(req1$, req2$, req3$).subscribe(value => console.log(`subscribe result: ${value}`))
+//parallelConcatFirst(req1$, req2$, req3$).subscribe(value => console.log(`subscribe result: ${value}`));
 
